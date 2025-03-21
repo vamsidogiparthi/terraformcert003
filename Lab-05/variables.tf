94efaa2ca82b76c1e170842788880181f468bd6c
Vamsi Dogiparthi <30088887@brighthorizons.com>
2025-03-20 21:31:51 -0400

    added variable changes

--- Lab-05/variables.tf
+++ Lab-05/variables.tf
@@ -14,6 +14,7 @@ variable "vpc_cidr" {
 }
 
 // name should be unique for each variable group in the variable block
+// variables with default values. The CLI will not prompt for values during terraform plan/apply
 variable "private_subnet" {
   default = {
     "private_subnet_1" = 0
@@ -34,14 +35,17 @@ variable "public_subnet" {
 variable "variables_sub_cidr" {
   description = "CIDR Block for the Variables Subnet"
   type        = string
+  default     = "10.0.202.0/24"
 }
 
 variable "variables_sub_az" {
-  description = "Availability Zone used Variables Subnet"
+  description = "Availability Zone used for Variables Subnet"
   type        = string
+  default     = "us-east-1a"
 }
 
 variable "variables_sub_auto_ip" {
   description = "Set Automatic IP Assigment for Variables Subnet"
   type        = bool
+  default     = true
 }
